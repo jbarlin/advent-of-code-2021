dcac37747a0c7935929ce6a992f35ef58ec33da6
jbarlin <jsb0910@hotmail.com>
2021-12-01 21:04:12 +1000

    Make output more interesting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,12 @@
 @main def hello: Unit = {
   println("Let's go AoC 2021!")
   //Day 1
-  print("Day 1:\n\tPart 1:\t")
-  println(time(Day1.partOne()))
+  print("Day 1:\n\tParse:\t")
+  val dayOneInput: List[Int] = time(Day1.parseInput())
+  print("\n\tPart 1:\t")
+  println(time(Day1.partOne(dayOneInput)))
   print("\tPart 2:\t")
-  println(time(Day1.partTwo()))
+  println(time(Day1.partTwo(dayOneInput)))
   //Day 2
   print("Day 2:")
 
@@ -18,5 +20,3 @@ def time[R](block: => R): R = {
     print("\t(Time:" + (t1 - t0) + "ns)\t")
     result
 }
-
-def msg = "I was compiled by Scala 3. :)"
