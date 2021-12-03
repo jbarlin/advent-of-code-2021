bc9844f27370bbe70bf3c2fb8dddc50071d108ac
jbarlin <jsb0910@hotmail.com>
2021-12-03 18:04:57 +1000

    Day 3, after some help

--- build.sbt
+++ build.sbt
@@ -1,12 +1,9 @@
 val scala3Version = "3.1.0"
 
 lazy val root = project
-  .in(file("."))
-  .settings(
-    name := "aoc",
-    version := "0.1.0-SNAPSHOT",
-
-    scalaVersion := scala3Version,
-
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
-  )
+    .in(file("."))
+    .settings(
+      name         := "aoc",
+      version      := "0.1.0-SNAPSHOT",
+      scalaVersion := scala3Version
+    )
--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -1,12 +1,14 @@
 import lib.DayTemplate
 import scala.io.Source
 import scala.math
+import scala.annotation.tailrec
 
 class Tally(val zeros: Int = 0, val ones: Int = 0) {
     def add(digit: Int): Tally = {
-        if (digit == 1) {
+        if digit == 1 then {
             Tally(this.zeros, this.ones + 1)
-        } else {
+        }
+        else {
             Tally(this.zeros + 1, this.ones)
         }
     }
@@ -20,17 +22,19 @@ class Tally(val zeros: Int = 0, val ones: Int = 0) {
     }
 
     def toLarger(): Int = {
-        if (this.ones > this.zeros) {
+        if this.ones > this.zeros then {
             1
-        } else {
+        }
+        else {
             0
         }
     }
 
     def toSmaller(): Int = {
-        if (this.ones < this.zeros) {
+        if this.ones < this.zeros then {
             1
-        } else {
+        }
+        else {
             0
         }
     }
@@ -40,22 +44,45 @@ class BinarySearcher(
     val larger: List[List[Int]],
     val smaller: List[List[Int]]
 ) {
-    def reduce(tally: Tally, index: Int): BinarySearcher = {
-        val larger  = if (this.larger.size <= 1) { this.larger }
-        else {
-            this.larger
-                .filter((innerBin) => {
-                    innerBin(index) == {if (!tally.isEqual()) {tally.toLarger()} else {1}}
-                })
+    @tailrec
+    final def reduce(index: Int): BinarySearcher = {
+        val largerTally  = this.larger.foldLeft(new Tally())((tally, row) => {
+            tally.add(row(index))
+        });
+        val larger       = {
+            if this.larger.size <= 1 then { this.larger }
+            else {
+                this.larger
+                    .filter((innerBin) => {
+                        innerBin(index) == {
+                            if !largerTally.isEqual() then { largerTally.toLarger() }
+                            else { 1 }
+                        }
+                    })
+            }
         }
-        val smaller = if (this.smaller.size <= 1) { this.smaller }
-        else {
-            this.smaller
-                .filter((innerBin) => {
-                    innerBin(index) == {if (!tally.isEqual()) {tally.toSmaller()} else {0}}
-                })
+        val smallerTally = this.smaller.foldLeft(new Tally())((tally, row) => {
+            tally.add(row(index))
+        });
+        val smaller      = {
+            if this.smaller.size <= 1 then { this.smaller }
+            else {
+                this.smaller
+                    .filter((innerBin) => {
+                        innerBin(index) == {
+                            if !smallerTally.isEqual() then { smallerTally.toSmaller() }
+                            else { 0 }
+                        }
+                    })
+            }
         }
-        new BinarySearcher(larger, smaller)
+        if larger.size == smaller.size && smaller.size == 1 then {
+            new BinarySearcher(larger, smaller)
+        }
+        else {
+            new BinarySearcher(larger, smaller).reduce(index + 1)
+        }
+
     }
 }
 
@@ -102,14 +129,11 @@ object Day3 extends DayTemplate[(List[Tally], List[List[Int]])] {
     }
 
     def partTwo(input: (List[Tally], List[List[Int]])): String = {
-        val tallies: List[Tally]      = input._1
         val binaries: List[List[Int]] = input._2
         val binSearch: BinarySearcher = new BinarySearcher(binaries, binaries);
-        val searched: BinarySearcher  = tallies.zipWithIndex
-            .foldLeft(binSearch)((curr, app) => curr.reduce(app._1, app._2))
+        val searched: BinarySearcher  = binSearch.reduce(0);
         val larger: String            = searched.larger.head.foldLeft("")((c, u) => c + u.toString)
         val smaller: String           = searched.smaller.head.foldLeft("")((c, u) => c + u.toString)
-        //3951264
         (Integer.parseInt(larger, 2) * Integer.parseInt(smaller, 2))
             .toString()
     }
