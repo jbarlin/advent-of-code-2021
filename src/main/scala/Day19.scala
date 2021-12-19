71dfee1d57dd059d8f27c0d708a29c85107973a9
jbarlin <jsb0910@hotmail.com>
2021-12-20 06:13:19 +1000

    Some code done on mobile while @ the platform
    Wonder if this will compile at all... Probably not!

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -1,4 +1,5 @@
 import org.apache.commons.lang3.builder.HashCodeBuilder
+import scala.collection.parallel.CollectionConverters._
 import scala.collection.SeqView
 
 final class Coord3(val x: Int, val y: Int, val z: Int) {
@@ -48,10 +49,16 @@ final class Coord3(val x: Int, val y: Int, val z: Int) {
 }
 
 final class Scanner(val beacons: Set[Coord3]) {
-    val orientations = beacons.toSeq.map(_.rotations).transpose.map(_.toSet)
 
-    def findOtherRelToMe(other: Scanner): Option[(Set[Coord3], Coord3)] = {
-        other.orientations.view
+    override def equals(o: Any): Boolean = o match {
+       case that: Scanner => that.beacons == beacons
+       case _ => false
+    }
+
+    lazy val orientations = beacons.toSeq.map(_.rotations).transpose.map(_.toSet)
+
+    def findOtherRelToMe(other: Scanner): Option[(Set[Coord3], Coord3, Scanner)] = {
+        other.orientations.par
             .flatMap(rotated => {
                 beacons.view.flatMap(beaconFrom1 => {
                     other.beacons
@@ -64,28 +71,45 @@ final class Scanner(val beacons: Set[Coord3]) {
             val intersect = rotated.map(_ + diff) & beacons
             (rotated, intersect, diff)
         })
-        .filter(f => f._2.size >= 12)
+        .find(f => f._2.size >= 12)
         //We need the rotations of these nodes, and the diff to get to this one?
-        .map(b => (b._1, b._3))
-        .headOption
+        .map(b => (b._1, b._3, other))
     }
 }
 
 object Scanner{
-
-    private def innerSolve(scanners: Seq[(Scanner, Int)], beacons: Set[Coord3]): Set[Coord3] = {
+    private def innerSolve(scanners: Seq[Scanner], beacons: Set[Coord3]): Set[Coord3] = {
         if (scanners.isEmpty){
             beacons
         }else{
             val baseScanner = new Scanner(beacons)
-            ???
+            val relative: (Set[Coord3], Coord3, Scanner) = scanners.view.map(sc => baseScanner.findOtherRelToMe(sc)).find(_.isDefined).get()
+            innerSolve(scanners.filter(_ != relative._3), beacons :+ relative._1.map(_ + relative._2))
         }
     }
 
     def solve(scanners: Seq[Scanner]): Set[Coord3] = {
-        val possibles = scanners.zipWithIndex
-        innerSolve(possibles.tail, possibles.head._1.beacons)
+        val possibles = scanners
+        innerSolve(possibles.tail, possibles.head.beacons)
     }
 }
 
-object Day19 {}
+object Day19 extends DayTemplate[Seq[Scanner]] {
+    def parseInput(): Seq[Scanner] = {
+         val inp = Source.readResource("day19-test-a.txt").getLines.foldLeft((Seq[Scanner], Set[Coords3]))((acc, ln) => {
+             if (ln.isEmpty || ln.head == '-' ) {
+                 (acc._1 :+ new Scanner(acc._2), Set.empty)
+             } else {
+                 val split = ln.split(',')
+                 val C = new Coord3(ln(0).toInt, ln(1).toInt, ln(2).toInt)
+                 (acc._1, acc._2 :+ C)
+             }
+
+         })
+         (inp._1 :+ new Scanner(inp._2)).filter(!_.beacons.isEmpty)
+    }
+
+    def partOne(input: Seq[Scanner]): String = Scanner.solve(input).size.toString
+
+    def partTwo(input: Seq[Scanner]): String = ""
+}
